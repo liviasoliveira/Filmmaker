93c084a80fedb2ab2c6f8b8c5bbf81171b9b86f6
Lívia Oliveira <lso2@academy.cin.ufpe.br>
2020-04-08 15:39:46 -0300

    collection view and fourth view

--- Filmmaker.playground/Contents.swift
+++ Filmmaker.playground/Contents.swift
@@ -7,7 +7,6 @@ let cfURL = Bundle.main.url(forResource: "SF-Compact-Display-Bold", withExtensio
 CTFontManagerRegisterFontsForURL(cfURL, CTFontManagerScope.process, nil)
 
 
-
 class FirstViewController : UIViewController {
     
     @IBAction func touchedButtonPlay() {
@@ -15,8 +14,6 @@ class FirstViewController : UIViewController {
         let vc = SecondViewController(screenType: .other(width: 750, height: 1024), isPortrait: true)
         //navigationController?.popViewController(animated: true)
         navigationController?.pushViewController(vc, animated: true)
-        
-        
     }
     
     override func loadView() {
@@ -80,17 +77,16 @@ class SecondViewController : UIViewController, UICollectionViewDelegate, UIColle
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Carta", for: indexPath) as? Carta else{
             fatalError("nao foi possivel acessar a celula")
         }
-       
         
         cell.imageCareer.image = UIImage(named: career.image)
         cell.labelCareer.text = career.name
-        
         
         if firstRound {
             cell.labelCareer.isHidden = true
             cell.imageCareer.isHidden = true
             cell.checkView.isHidden = true
         }
+        
         //        cell.addTarget(self, action: #selector(virarCarta), for: .touchUpInside)
         //        self.view.frame = CGRect(x: career.x, career.y: y, width: 273, height: 321)
         
@@ -98,16 +94,15 @@ class SecondViewController : UIViewController, UICollectionViewDelegate, UIColle
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 271, height: 321)
+        return CGSize(width: 273, height: 321)
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         firstRound = false
         let career = Career.shared[indexPath.row]
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Carta", for: indexPath) as? Carta else{
+        guard let cell = collectionView.cellForItem(at: indexPath) as? Carta else{
             fatalError("nao foi possivel acessar a celula")
         }
-        
         
         cell.imageCareer.image = UIImage(named: career.image)
         cell.labelCareer.text = career.name
@@ -162,7 +157,7 @@ class SecondViewController : UIViewController, UICollectionViewDelegate, UIColle
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
         layout.sectionInset = UIEdgeInsets(top: 50, left:60, bottom:60, right:60)
-        layout.itemSize = CGSize(width: 271, height: 321)
+        layout.itemSize = CGSize(width: 273, height: 321)
         cartaCollection = UICollectionView(frame: frame, collectionViewLayout: layout)
         cartaCollection?.delegate = self
         cartaCollection?.dataSource = self
@@ -183,6 +178,112 @@ class SecondViewController : UIViewController, UICollectionViewDelegate, UIColle
     }
 }
 
+class ThirdViewController: UIViewController {
+    
+    override func loadView() {
+    let view = UIView()
+    view.backgroundColor = .white
+        
+        let imgBackground = UIImageView(frame: CGRect(x: 0, y: 0, width: 750, height: 1024))
+        imgBackground.image = UIImage(imageLiteralResourceName: "vitoria.png")
+        
+        let textovitoria = UILabel()
+        textovitoria.frame = CGRect(x: 80, y: 143, width: 615, height: 53)
+        textovitoria.text = "Nós ganhamos o Melhor Filme!"
+        textovitoria.textColor = #colorLiteral(red: 0.0862745098, green: 0.1450980392, blue: 0.4117647059, alpha: 1)
+        textovitoria.font = UIFont(name: "SF Compact Display", size: 44)
+        
+        let textovitoria2 = UILabel()
+        textovitoria2.frame = CGRect(x: 130, y: 200, width: 600, height: 120)
+        textovitoria2.text = "Não teríamos conseguido sem você.\n               Vamos ver como ficou?"
+        textovitoria2.numberOfLines = 3
+        textovitoria2.textColor = #colorLiteral(red: 0.0862745098, green: 0.1450980392, blue: 0.4117647059, alpha: 1)
+        textovitoria2.font = UIFont.systemFont(ofSize: 30, weight: UIFont.Weight.semibold)
+        
+        let labelConfirmar = UILabel()
+        labelConfirmar.frame = CGRect(x: 303, y: 902, width: 137, height: 36)
+        labelConfirmar.text = "Confirmar"
+        labelConfirmar.textColor = .white
+        labelConfirmar.font = UIFont.systemFont(ofSize: 30, weight: UIFont.Weight.semibold)
+        
+        let buttonConfirmar = UIButton()
+        buttonConfirmar.frame = CGRect(x: 265, y: 893, width: 210, height: 55)
+        
+        let imageButtonConfirmar = UIImage(named: "botao.png")
+        buttonConfirmar.setImage(imageButtonConfirmar, for: .normal)
+        
+        buttonConfirmar.addTarget(self, action: #selector(ThirdViewController.touchedButtonConfirmar), for: .touchUpInside)
+        
+        view.addSubview(imgBackground)
+        view.addSubview(textovitoria)
+        view.addSubview(textovitoria2)
+        view.addSubview(buttonConfirmar)
+        view.addSubview(labelConfirmar)
+        
+        self.view = view
+             
+         }
+    
+        @IBAction func touchedButtonConfirmar() {
+               print("tocou botão confirmar")
+            let vc = FourthViewController(screenType: .other(width: 750, height: 1024), isPortrait: true)
+            self.navigationController?.pushViewController(vc, animated: true)
+           }
+    
+}
+
+class FourthViewController: UIViewController {
+    
+    override func loadView() {
+       let view = UIView()
+       view.backgroundColor = .white
+        
+        let imgBackground = UIImageView(frame: CGRect(x: 0, y: 0, width: 750, height: 1024))
+        imgBackground.image = UIImage(imageLiteralResourceName: "film.png")
+        
+        let scene = UIImageView(frame: CGRect(x: 100, y: 266, width: 565, height: 370))
+        scene.image = UIImage(imageLiteralResourceName: "scene.png")
+        
+        let labelRecomecar = UILabel()
+        labelRecomecar.frame = CGRect(x: 310, y: 843, width: 153, height: 36)
+        labelRecomecar.text = "Recomeçar"
+        labelRecomecar.textColor = .white
+        labelRecomecar.font = UIFont.systemFont(ofSize: 28, weight: UIFont.Weight.semibold)
+        
+        let buttonRecomecar = UIButton()
+        buttonRecomecar.frame = CGRect(x: 279, y: 833, width: 210, height: 55)
+        
+        let imageButtonRecomecar = UIImage(named: "botao.png")
+        buttonRecomecar.setImage(imageButtonRecomecar, for: .normal)
+        
+        buttonRecomecar.addTarget(self, action: #selector(FourthViewController.touchedButtonRecomecar), for: .touchUpInside)
+        
+        let playFilm = UIButton()
+        playFilm.frame = CGRect(x: 338, y: 420, width: 66, height: 54)
+        
+        let imagePlayFilm = UIImage(named: "playFilm.png")
+        playFilm.setImage(imagePlayFilm, for: .normal)
+        
+        playFilm.addTarget(self, action: #selector(FourthViewController.touchedButtonRecomecar), for: .touchUpInside)
+        
+        view.addSubview(imgBackground)
+        view.addSubview(scene)
+        view.addSubview(labelRecomecar)
+        view.addSubview(buttonRecomecar)
+        view.addSubview(labelRecomecar)
+        view.addSubview(playFilm)
+        
+        self.view = view
+        
+             }
+        
+            @IBAction func touchedButtonRecomecar() {
+                   print("tocou botão recomeçar")
+                let vc = FirstViewController(screenType: .other(width: 750, height: 1024), isPortrait: true)
+                self.navigationController?.pushViewController(vc, animated: true)
+               }
+}
+
 // Present the view controller in the Live View window
 public enum SFCompactDisplay: String {
     case typewriter = "SF-Compact-Display-Regular"
@@ -199,3 +300,4 @@ let navigationController = UINavigationController(screenType: .other(width: 750,
 navigationController.pushViewController(vc, animated: true)
 navigationController.navigationBar.isHidden = true
 PlaygroundPage.current.liveView = navigationController.scale(to: 0.4)
+
--- Filmmaker.playground/Sources/Carta.swift
+++ Filmmaker.playground/Sources/Carta.swift
@@ -20,7 +20,7 @@ public class Career{
      Career(career: "Cenografista", image: "cenografista.png", x: 87, y: 277),
      Career(career: "Continuísta", image: "continuista.png", x: 411, y: 277),
      Career(career: "Montadora", image: "montadora.png", x: 87, y: 622),
-     Career(career: "Direção de Fotografia", image: "direcaoDeFotografia.png", x: 411, y: 622),
+     Career(career: "Direção de Fotografia", image: "diretorDeFotografia.png", x: 411, y: 622),
      Career(career: "Figurinista", image: "figurinista.png", x: 87, y: 967),
      Career(career: "Trilheira", image: "trilheira.png", x: 411, y: 967),
     ]
@@ -43,18 +43,23 @@ public class Carta: UICollectionViewCell {
         
         self.view.setImage(background, for: .normal)
 
-        self.labelCareer.frame = CGRect(x: 472, y: 865, width: 146, height: 60)
+        self.labelCareer.frame = CGRect(x: 30, y: 260, width: 146, height: 60)
 
-        self.imageCareer.frame = CGRect(x: 491, y: 639, width: 120, height: 211)
+        self.imageCareer.frame = CGRect(x: 30, y: 30, width: 120, height: 211)
         
         self.checkView.image = UIImage(named: "check.png")
-        self.checkView.frame = CGRect(x: 616, y: 655, width: 40, height: 40)
+        self.checkView.frame = CGRect(x: 30, y: 30, width: 40, height: 40)
         
         self.addSubview(UIImageView(image:background))
         self.contentView.addSubview(labelCareer)
         self.contentView.addSubview(imageCareer)
-        self.view.addSubview(labelCareer)
-        self.view.addSubview(checkView)
+//        self.view.addSubview(labelCareer)
+        self.contentView.addSubview(checkView)
+        
+        self.bringSubviewToFront(contentView)
+        self.contentView.bringSubviewToFront(imageCareer)
+        self.contentView.bringSubviewToFront(labelCareer)
+        self.contentView.bringSubviewToFront(checkView)
 
     }
     
